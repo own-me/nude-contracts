ea971ded0609d30e94999e9cdee5dcf220722be6
Christopher Trimboli <christopher.trimboli@protonmail.com>
2021-07-22 22:13:11 -0700

    deploy NudeNFT

--- migrations/1_deploy_contract.js
+++ migrations/1_deploy_contract.js
@@ -1,5 +1,7 @@
 const Nude = artifacts.require("Nude");
+const NudeNFT = artifacts.require("NudeNFT");
 
 module.exports = function(deployer) {
   deployer.deploy(Nude, 1000);
+  deployer.deploy(NudeNFT);
 };
