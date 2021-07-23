021def065aa9f51e5acd57c47d844b0f6bd10886
Christopher Trimboli <christopher.trimboli@protonmail.com>
2021-07-22 23:03:24 -0700

    new build, fix names

--- contracts/NudeNFT.sol
+++ contracts/NudeNFT.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.2;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
-contract MyToken is ERC721, ERC721URIStorage {
+contract NudeNFT is ERC721, ERC721URIStorage {
     constructor() ERC721("NudeNFT", "NUDENFT") {}
 
     function _burn(uint256 tokenId)
