40f8acaca60762091093f30e76374555c4075d33
Christopher Trimboli <christopher.trimboli@protonmail.com>
2021-09-06 23:09:03 -0700

    fix NUDE initial supply and params

--- contracts/Nude.sol
+++ contracts/Nude.sol
@@ -5,10 +5,10 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Nude is ERC20 {
     address private owner;
-    mapping (address => uint256) balances;
+    uint256 public initialSupply = 69696969;
 
-    constructor(uint256 initialSupply) ERC20("Nude", "NUDE") {
-        _mint(msg.sender, initialSupply);
+    constructor() ERC20("Nude", "NUDE") {
+        _mint(msg.sender, initialSupply * (10 ** decimals()));
         owner = msg.sender; 
     }
 }
--- migrations/1_deploy_contract.js
+++ migrations/1_deploy_contract.js
@@ -2,6 +2,6 @@ const Nude = artifacts.require("Nude");
 const NudeNFT = artifacts.require("NudeNFT");
 
 module.exports = function(deployer) {
-  deployer.deploy(Nude, 69696969);
+  deployer.deploy(Nude);
   deployer.deploy(NudeNFT);
 };
