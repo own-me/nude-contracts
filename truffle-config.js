8e45bf13530a35ee41fa2cc59edc725bc01d3e3f
Christopher Trimboli <christopher.trimboli@protonmail.com>
2021-09-06 21:06:36 -0700

    use secrets to deploy

--- truffle-config.js
+++ truffle-config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HDWalletProvider = require('@truffle/hdwallet-provider');
+const { infuraProjectId, mnemonic } = require('./secrets.json');
 
 module.exports = {
     contracts_build_directory: path.join(__dirname, "build"),
@@ -10,7 +11,7 @@ module.exports = {
             network_id: "*",
         },
         ropsten: {
-            provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/${projectId}`),
+            provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/${infuraProjectId}`),
             network_id: 3,
             gas: 5500000,
             confirmations: 2,
