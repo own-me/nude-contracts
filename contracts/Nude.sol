86292db0f6fa7a8e036a7ab183deea25865548d5
Christopher Trimboli <christopher.trimboli@protonmail.com>
2022-02-13 09:54:23 -0800

    add contract header comments

--- contracts/Nude.sol
+++ contracts/Nude.sol
@@ -1,4 +1,8 @@
 // SPDX-License-Identifier: BSD 3-Clause
+// Own Me Inc. -CJFT
+// www.ownme.io
+// Nude. MATIC Polygon ERC20 in-app currency and governance token that powers our home for adult content in the web3 metaverse.
+
 pragma solidity ^0.8.11;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
