881d7656de42bb21e28818e12cf399a08c2f209e
Yan Wei <im.wayne.wy@gmail.com>
2022-05-27 14:09:20 -0700

    add local deploy

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -3,14 +3,19 @@ import { ethers } from "hardhat";
 async function main() {
   const Nude = await ethers.getContractFactory("Nude");
   const NudeNFT = await ethers.getContractFactory("NudeNFT");
+  const NudeDEX = await ethers.getContractFactory("NudeDEX");
 
   const nude = await Nude.deploy();
   await nude.deployed();
   console.log("NUDE deployed to:", nude.address);
 
-  const nudeNFT = await NudeNFT.deploy(nude.address);
+  const nudeNFT = await NudeNFT.deploy();
   await nudeNFT.deployed();
   console.log("NUDE_NFT deployed to:", nudeNFT.address);
+
+  const nudeDEX = await NudeDEX.deploy(nude.address, nudeNFT.address);
+  await nudeDEX.deployed();
+  console.log("NUDE_DEX deployed to:", nudeDEX.address);
 }
 
 main().catch((error) => {
