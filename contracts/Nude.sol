ea38f48af3daed5da2e40952ebf761b0ce848c40
Christopher Trimboli <christopher.trimboli@protonmail.com>
2022-06-05 22:20:55 -0600

    tune-up NUDE contract functions, linting, tokenRate public

--- contracts/Nude.sol
+++ contracts/Nude.sol
@@ -10,41 +10,33 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "./NudeDEX.sol";
 
 contract Nude is ERC20, Ownable {
-
     uint256 public initialSupply = 69696969;
     uint256 public tokensSold;
-    uint256 private tokenRate = 10; // how many tokens can bought with 1 eth
+    uint256 public tokenRate = 10; // how many tokens can bought with 1 eth
     NudeDEX private dexContract;
     uint8 public tax = 10;
-
 
     event Sell(address _buyer, uint256 _amount);
 
     constructor() ERC20("Nude", "NUDE") {
-        _mint(msg.sender, initialSupply * (10 ** decimals()));
+        _mint(msg.sender, initialSupply * (10**decimals()));
+    }
+
+    function setNudeDex(address dexAddress) external onlyOwner {
+        dexContract = NudeDEX(dexAddress);
     }
 
     function buyTokens(uint256 _numberOfTokens) external payable {
-        require(msg.value * tokenRate == _numberOfTokens * (10 ** decimals()), "Not exact amount");
+        require(msg.value * tokenRate == _numberOfTokens * (10**decimals()), "Not exact amount");
         require(balanceOf(owner()) >= _numberOfTokens, "Not enough tokens");
         _transfer(owner(), msg.sender, _numberOfTokens);
         tokensSold += _numberOfTokens;
         emit Sell(msg.sender, _numberOfTokens);
     }
 
-    function transferTokens(address from, address to, uint256 amount ) external {
+    function transferTokens(address from, address to, uint256 amount) external {
         require(balanceOf(from) >= amount, "Not enough tokens");
         _transfer(from, to, amount);
-    }
-
-    function setTokenRate(uint256 _rate) external onlyOwner {
-        tokenRate = _rate;
-    }
-    function getTokenRate() external view returns (uint256) {
-        return tokenRate;
-    }
-    function setNudeDex(address dexAddress) external onlyOwner {
-        dexContract = NudeDEX(dexAddress);
     }
 
     function approveAndBuyNFT(uint256 tokenId) external {
@@ -52,6 +44,4 @@ contract Nude is ERC20, Ownable {
         super.approve(address(dexContract), (price * (100 + tax)) / 100);
         dexContract.trade(tokenId, msg.sender);
     }
-
-    // todo: implement sell tokens function
 }
