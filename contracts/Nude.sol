836efc467df8955d72ef7989a60c35c7de627886
Yan Wei <im.wayne.wy@gmail.com>
2022-06-08 15:56:58 -0700

    init dao contracts

--- contracts/Nude.sol
+++ contracts/Nude.sol
@@ -6,10 +6,12 @@
 pragma solidity ^0.8.14;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "./NudeDEX.sol";
 
-contract Nude is ERC20, Ownable {
+contract Nude is ERC20, ERC20Votes, Ownable {
+
     uint256 public initialSupply = 69696969;
     uint256 public tokensSold;
     uint256 public tokenRate = 10; // how many tokens can bought with 1 eth
@@ -51,4 +53,22 @@ contract Nude is ERC20, Ownable {
         super.approve(address(dexContract), (price * (100 + tax)) / 100);
         dexContract.trade(tokenId, msg.sender);
     }
+
+    // override functions for ERC20Votes
+    function _afterTokenTransfer(
+        address from,
+        address to,
+        uint256 amount
+    ) internal override(ERC20, ERC20Votes) {
+        super._afterTokenTransfer(from, to, amount);
+    }
+
+    function _mint(address to, uint256 amount) internal override(ERC20, ERC20Votes) {
+        super._mint(to, amount);
+    }
+
+    function _burn(address account, uint256 amount) internal override(ERC20, ERC20Votes) {
+        super._burn(account, amount);
+    }
+    // todo: implement sell tokens function
 }
